93145d606025ebe674037b686641575963be920f
Anthony (Codetoil) <ianthisawesomee@gmail.com>
2023-09-12 06:53:45 -0400

    Re-added slim mode support

--- litlaunch-library-api/test/src/main/c/run.c
+++ litlaunch-library-api/test/src/main/c/run.c
@@ -9,22 +9,41 @@ int main()
     printf("EMBEDDED_DEP: %i\n", EMBEDDED_DEP);
 
     Module* apiModule = initLibraryApi();
-    
+
+#ifndef _LITLAUNCH_SLIM_
     const char* libApiModuleLocStr = resourceLocationToString(apiModule->id);
     const char* libApiModuleVersionLocStr = resourceLocationToString(apiModule->version->id);
     const char* libApiModuleVersionValue = getVersionString(apiModule->version);
+#else
+    char libApiModuleLocNamespace = getResourceLocationNamespace(apiModule->id);
+    char libApiModuleLocPath = getResourceLocationPath(apiModule->id);
+    char libApiModuleVersionLocNamespace = getResourceLocationNamespace(apiModule->version->id);
+    char libApiModuleVersionLocPath = getResourceLocationPath(apiModule->version->id);
+    char libApiModuleVersionValue = apiModule->version->version;
+#endif
 
     printf("LitLaunch Library API Implementation:\n");
+#ifndef _LITLAUNCH_SLIM_
     printf("\tResource Location: %s\n", libApiModuleLocStr);
+#else
+    printf("\tResource Location: %x%x\n", libApiModuleLocNamespace, libApiModuleLocPath);
+#endif
     printf("\tVersion:\n");
+#ifndef _LITLAUNCH_SLIM_
     printf("\t\tResource Location: %s\n", libApiModuleVersionLocStr);
     printf("\t\tValue: %s\n", libApiModuleVersionValue);
+#else
+    printf("\t\tResource Location: %x%x\n", libApiModuleVersionLocNamespace, libApiModuleVersionLocPath);
+    printf("\t\tValue: %x\n", libApiModuleVersionValue);
+#endif
     printf("\tDependencies:\n");
     printf("\n");
     
     freeVersion(apiModule->version);
+#ifndef _LITLAUNCH_SLIM_
     free(libApiModuleVersionLocStr);
     free(libApiModuleLocStr);
+#endif
     freeResourceLocation(apiModule->id);
     freeModule(apiModule);
     return 0;
